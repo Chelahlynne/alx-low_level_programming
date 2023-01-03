97f5319c5df6cfac4a81098cd861424d90a1346a
Chelahlynne <lynnejelagatkipsang@gmail.com>
2023-01-02 18:50:24 -0800

    m

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - main block
+ * main - main function
  * Description: prints all single number of base 10
  * sarting from 0, followed y a new line
  * Return: 0
